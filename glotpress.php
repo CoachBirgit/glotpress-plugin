4f89e8edf6dc81dcf3ed6a7550219880acf7484c
Marko Heijnen <info@markoheijnen.com>
2013-01-30 15:13:23 +0100

    First stuff based on BuddyPress

---
+++ glotpress.php
@@ -0,0 +1,210 @@
+<?php
+
+/**
+ * The GlotPress Plugin
+ *
+ * GlotPress is a collaborative, web-based software translation tool.
+ *
+ * $Id$
+ *
+ * @package GlotPress
+ * @subpackage Main
+ */
+
+/**
+ * Plugin Name: GlotPress
+ * Plugin URI:  http://glotpress.org
+ * Description: GlotPress is a collaborative, web-based software translation tool.
+ * Author:      The GlotPress Community
+ * Author URI:  http://glotpress.org
+ * Version:     0.1-bleeding
+ * Text Domain: glotpress
+ * Domain Path: /languages/
+ */
+
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+if ( !class_exists( 'GlotPress' ) ) :
+/**
+ * Main GlotPress Class
+ */
+final class GlotPress {
+
+	/**
+	 * @var GlotPress The one true GlotPress
+	 */
+	private static $instance;
+
+	/**
+	 * Main GlotPress Instance
+	 *
+	 * Insures that only one instance of GlotPress exists in memory at any one
+	 * time. Also prevents needing to define globals all over the place.
+	 *
+	 * @since 0.1
+	 * @static var array $instance
+	 * @uses GlotPress::setup_globals() Setup the globals needed
+	 * @uses GlotPress::includes() Include the required files
+	 * @uses GlotPress::setup_actions() Setup the hooks and actions
+	 * @see glotpress()
+	 * @return The one true GlotPress
+	 */
+	public static function instance() {
+		if ( ! isset( self::$instance ) ) {
+			self::$instance = new GlotPress;
+			self::$instance->setup_globals();
+			self::$instance->includes();
+			self::$instance->setup_actions();
+		}
+		return self::$instance;
+	}
+
+	/** Magic Methods *********************************************************/
+
+	/**
+	 * A dummy constructor to prevent GlotPress from being loaded more than once.
+	 *
+	 * @since 0.1
+	 * @see GlotPress::instance()
+	 * @see glotpress();
+	 */
+	private function __construct() { /* Do nothing here */ }
+
+	/**
+	 * A dummy magic method to prevent GlotPress from being cloned
+	 *
+	 * @since 0.1
+	 */
+	public function __clone() { _doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'glotpress' ), '0.1' ); }
+
+	/**
+	 * A dummy magic method to prevent GlotPress from being unserialized
+	 *
+	 * @since 0.1
+	 */
+	public function __wakeup() { _doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'glotpress' ), '0.1' ); }
+
+	/**
+	 * Magic method for checking the existence of a certain custom field
+	 *
+	 * @since 0.1
+	 */
+	public function __isset( $key ) { return isset( $this->data[$key] ); }
+
+	/**
+	 * Magic method for getting GlotPress variables
+	 *
+	 * @since 0.1
+	 */
+	public function __get( $key ) { return isset( $this->data[$key] ) ? $this->data[$key] : null; }
+
+	/**
+	 * Magic method for setting GlotPress variables
+	 *
+	 * @since 0.1
+	 */
+	public function __set( $key, $value ) { $this->data[$key] = $value; }
+
+	/**
+	 * Magic method for unsetting GlotPress variables
+	 *
+	 * @since 0.1
+	 */
+	public function __unset( $key ) { if ( isset( $this->data[$key] ) ) unset( $this->data[$key] ); }
+
+	/**
+	 * Magic method to prevent notices and errors from invalid method calls
+	 *
+	 * @since 0.1
+	 */
+	public function __call( $name = '', $args = array() ) { unset( $name, $args ); return null; }
+
+	/**
+	 * Set some smart defaults to class variables. Allow some of them to be
+	 * filtered to allow for early overriding.
+	 *
+	 * @since 0.1
+	 * @access private
+	 * @uses plugin_dir_path() To generate GlotPress plugin path
+	 * @uses plugin_dir_url() To generate GlotPress plugin url
+	 * @uses apply_filters() Calls various filters
+	 */
+	private function setup_globals() {
+
+		/** Versions **********************************************************/
+
+		$this->version    = '0.1';
+		$this->db_version = '1';
+
+		/** Paths *************************************************************/
+
+		// Setup some base path and URL information
+		$this->file       = __FILE__;
+		$this->basename   = apply_filters( 'gp_plugin_basenname', plugin_basename( $this->file ) );
+		$this->plugin_dir = apply_filters( 'gp_plugin_dir_path',  plugin_dir_path( $this->file ) );
+		$this->plugin_url = apply_filters( 'gp_plugin_dir_url',   plugin_dir_url ( $this->file ) );
+
+		// Includes
+		$this->includes_dir = apply_filters( 'gp_includes_dir', trailingslashit( $this->plugin_dir . 'includes'  ) );
+		$this->includes_url = apply_filters( 'gp_includes_url', trailingslashit( $this->plugin_url . 'includes'  ) );
+
+		// Languages
+		$this->lang_dir     = apply_filters( 'gp_lang_dir',     trailingslashit( $this->plugin_dir . 'languages' ) );
+
+		// Templates
+		$this->themes_dir   = apply_filters( 'gp_themes_dir',   trailingslashit( $this->plugin_dir . 'templates' ) );
+		$this->themes_url   = apply_filters( 'gp_themes_url',   trailingslashit( $this->plugin_url . 'templates' ) );
+
+		/** Misc **************************************************************/
+
+		$this->domain         = 'glotpress';      // Unique identifier for retrieving translated strings
+
+		/** Cache *************************************************************/
+
+		// Add GlotPress to global cache groups
+		wp_cache_add_global_groups( 'glotpress' );
+	}
+
+	/**
+	 * Include required files
+	 *
+	 * @since 0.1
+	 * @access private
+	 * @uses is_admin() If in WordPress admin, load additional file
+	 */
+	private function includes() {
+
+	}
+
+	/**
+	 * Setup the default hooks and actions
+	 *
+	 * @since 0.1
+	 * @access private
+	 * @uses add_action() To add various actions
+	 */
+	private function setup_actions() {
+		
+	}
+
+}
+
+/**
+ * The main function responsible for returning the one true GlotPress Instance
+ * to functions everywhere.
+ *
+ * Use this function like you would a global variable, except without needing
+ * to declare the global.
+ *
+ * Example: <?php $gp = glotpress(); ?>
+ *
+ * @return The one true GlotPress Instance
+ */
+function glotpress() {
+	return glotpress::instance();
+}
+
+glotpress();
+
+endif; // class_exists check
---
+++ readme.txt
@@ -0,0 +1,36 @@
+=== GlotPress ===
+Contributors: markoheijnen
+Tags: translation
+Requires at least: 3.5
+Tested up to: 3.5.1
+Stable tag: 0.1-bleeding
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+GlotPress is a collaborative, web-based software translation tool.
+
+== Description ==
+
+GlotPress is a collaborative, web-based software translation tool.
+
+== Installation ==
+
+1. Upload `glotpress` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Go to the GlotPress to start configuring
+
+== Frequently Asked Questions ==
+
+
+
+== Screenshots ==
+
+
+
+== Changelog ==
+
+= 0.1 =
+* First version
+
+== Upgrade Notice ==
+
