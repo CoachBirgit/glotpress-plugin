fa1cf8c047f9a4aea87c34bc4025511d38aa3203
Vinicius Massuchetto <vmassuchetto@gmail.com>
2013-02-13 00:58:55 -0200

    New set of rules for projects and sets
    In this new proposal we're trying to preserve the `gp_action` query_var
    with the method names from GP_Route_{entity}::{method} in GlotPress'
    `router.php`, and let it control the query part.  Post actions have
    always a `/post/` in the end.
    
    From this, we can also add subprojects support building a 'project dir
    parser' for `$project_match`. Although I don't think it's elegant to keep
    this parser logic here.
    
    See ticket #3

--- includes/router.php
+++ includes/router.php
@@ -11,17 +11,41 @@ class GlotPress_Router {
 	 * @see http://glotpress.trac.wordpress.org/browser/trunk/gp-includes/router.php
 	 *
 	 * @todo import get:/$project/-permissions/-delete/$dir and all $set stuff
+	 * @todo project match parser
 	 */
 	function rewrite_rules( $current_rules = array() ) {
+		$dir = '([^_/][^/]*)';
+		$path = '(.+?)';
+		$project = $path;
+		$project_match = 'gp_project=$matches[1]';
+		$id = '(\d+)';
+		$locale = '('.implode('|', array_map( create_function( '$x', 'return $x->slug;' ), GP_Locales::locales() ) ).')';
+		$set = "$project/$locale/$dir";
+		$set_match = 'gp_project=$matches[1]&gp_locale=$matches[2]&gp_type=$matches[3]';
 		$new_rules = array (
-			'login/?$'  => 'index.php?glotpress=login',
-			'profile/?$'  => 'index.php?glotpress=profile',
 
-			'projects/([^/]+)/-mass-create-sets/preview/?$' => 'index.php?glotpress=project&gp_project=$matches[1]&gp_type=-mass-create-sets-preview',
-			'projects/([^/]+)/(import-originals|-edit|-delete|-personal-permissions|-mass-create-sets)/?$' => 'index.php?glotpress=project&gp_project=$matches[1]&gp_type=$matches[2]',
-			'projects/([^/]+)/?$' => 'index.php?glotpress=project&gp_project=$matches[1]',
-			'projects/-new/?$'		 => 'index.php?glotpress=project&gp_type=new',
-			'projects/?$'		 => 'index.php?glotpress=project'
+			"new?" => 'index.php?gp_action=new_get',
+			"new/post/?" => 'index.php?gp_action=new_post',
+
+			'profile/?$' => 'index.php?gp_action=profile_get',
+			'profile/post/?$' => 'index.php?gp_action=profile_post',
+			"profile/$dir/?$" => 'index.php?gp_action=profile_get&gp_profile=$matches[1]',
+
+			"$project/import-originals/?" => "index.php?gp_action=import_originals_get&$project_match",
+			"$project/import-originals/post/?" => "index.php?gp_action=import_originals_post&$project_match",
+
+			"$project/edit/?" => "index.php?gp_action=edit_get&$project_match",
+			"$project/edit/post/?" => "index.php?gp_action=edit_post&$project_match",
+
+			"$project/delete/?" => "index.php?gp_action=delete_get&$project_match",
+			"$project/delete/post/?" => "index.php?gp_action=delete_post&$project_match",
+
+			"$project/permissions/?" => "index.php?gp_action=permissions_get&$project_match",
+			"$project/permissions/post/?" => "index.php?gp_action=permissions_post&$project_match",
+
+			"$set/?" => "index.php?gp_action=set_get&$set_match",
+			"$set/post/?" => "index.php?gp_action=set_post&$set_match"
+
 		);
 		return apply_filters( 'gp_rewrite_rules', $new_rules + $current_rules );
 	}
@@ -34,10 +58,10 @@ class GlotPress_Router {
 	 * @return New set of query vars
 	 */
 	function query_vars( $vars ) {
-		$vars[] = 'glotpress';
-		$vars[] = 'gp_type';
+		$vars[] = 'gp_action';
 		$vars[] = 'gp_project';
-		
+		$vars[] = 'gp_profile';
+		$vars[] = 'gp_locale';
 		return $vars;
 	}
 
@@ -45,16 +69,34 @@ class GlotPress_Router {
 	 * Redirect to query according to the query vars
 	 *
 	 * @since 0.1
+	 *
+	 * @todo all other matches
 	 */
 	function pre_get_posts() {
-		if( 'project' == get_query_var( 'glotpress' ) ) {
-			if( $project = get_query_var( 'gp_project' ) )
-				return gp_project( $project );
-			else
-				gp_projects_home();
+		switch( get_query_var( 'gp_action' ) ) {
+
+			case 'profile_get' :
+			break;
+
+			case 'profile_post' :
+			break;
+
+			case 'new_get' :
+			break;
+
+			case 'new_post' :
+			break;
+
+			case 'import_originals_get' :
+			break;
+
+			case 'import_originals_post' :
+			break;
+
+			case 'set_get' :
+			break;
+
 		}
-		elseif( 'profile' == get_query_var( 'glotpress' ) )
-			return gp_profile();
 	}
 }
 ?>
